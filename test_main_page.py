81444e8b91be79356f1c3bb8d2981213151db5f7
leha0794 <leha0794@mail.ru>
2020-09-04 13:23:06 +0300

    Fixeds atribut for Loginpage

--- test_main_page.py
+++ test_main_page.py
@@ -4,11 +4,11 @@ from .pages.login_page import LoginPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
-    x = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
 
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
-    x.should_be_register_form()
-    x.should_be_login_form()
-    x.should_be_login_url()
+    login_page.should_be_register_form()
+    login_page.should_be_login_form()
+    login_page.should_be_login_url()
